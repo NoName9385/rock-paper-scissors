cb2937f63fffe9d5d0e682a4500c608bef097bdd
NoName9385 <tlaw9385@gmail.com>
2024-08-09 09:29:27 +0800

    改名

--- src/rock-paper-scissors.js
+++ src/rock-paper-scissors.js
@@ -1,5 +1,5 @@
 // This would be stored in the 'src' folder of the GitHub repository
-// whack-a-mole.js
+// rock-paper-scissors.js
 window.initGame = (React, assetsUrl) => {
   const { useState, useEffect } = React;
 
